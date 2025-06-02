10c85ded9cb2dd64c54b66f3e6aa5ed0807723fa
Krishan <krishna.mahto@codingworkx.com>
2025-06-02 23:34:07 +0530

    added hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
+import Hero from "@/components/Hero";
+
 export default function Home() {
-  return <div className="flex  bg-black-100 text-white">Home page</div>;
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        {/* <FloatingNav navItems={navItems} /> */}
+        <Hero />
+        {/* <Grid />
+        <RecentProjects />
+        <Clients />
+        <Experience />
+        <Approach />
+        <Footer /> */}
+      </div>
+    </main>
+  );
 }
---
+++ src/components/GridBackground.tsx
@@ -0,0 +1,25 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+export function GridBackground() {
+  return (
+    <div className=" absolute top-0 left-0 h-screen w-screen flex  items-center justify-center bg-white dark:bg-black-100">
+      <div
+        className={cn(
+          "absolute inset-0",
+          "[background-size:40px_40px]",
+          "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
+          "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]"
+        )}
+      />
+      {/* Radial gradient for the container to give a faded look */}
+      <div
+        className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white 
+      [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black-100"
+      />
+      {/* <p className="relative z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text py-8 text-4xl font-bold text-transparent sm:text-7xl">
+        Backgrounds
+      </p> */}
+    </div>
+  );
+}
